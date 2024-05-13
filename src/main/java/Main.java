2618223a0dbe2c58ca77f1eeb9ef68d46a568928
LeonardoTiussMoura <167460740+LeonardoTiussMoura@users.noreply.github.com>
2024-05-13 20:52:42 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("---------------");
+    System.out.println("Stranger Things");
+    System.out.println("---------------");
+    System.out.println("");
+    System.out.println("Criado pelos Duffer Brothers (Irmãos Duffers), lançada em 2016, essa série conta com superpoderes mentais, controle psiquico sobre as coisas, na primeira temporada um garoto some do nada, barulhos, portais, coisas sobrenaturais acontecem, o que será que está por vir?");
+    System.out.println("");
+      System.out.println("Aqui está um link com o trailer da primeira temporada de Stranger Things:");
+    System.out.println("https://www.youtube.com/watch?v=b9EkMc79ZSU");
+  }
+}
